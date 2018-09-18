9418f60dad15454bb230d8fab49ab24053286d42
AMIT MAHAJAN <39582912+amitmahajan993@users.noreply.github.com>
2018-09-18 09:37:41 +0530

    Create InversionCount.cpp
    Inversion Count for an array indicates – how far (or close) the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum. 
    Formally speaking, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j

---
+++ InversionCount.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int getInvCount(int arr[], int n) 
+{ 
+int inv_count = 0; 
+for (int i = 0; i < n - 1; i++) 
+	for (int j = i+1; j < n; j++) 
+	if (arr[i] > arr[j]) 
+		inv_count++; 
+
+return inv_count; 
+} 
+
+/* Driver progra to test above functions */
+int main(int argv, char** args) 
+{ 
+int arr[] = {1, 20, 6, 4, 5}; 
+int n = sizeof(arr)/sizeof(arr[0]); 
+cout<<" Number of inversions are " <<getInvCount(arr, n)<<endl; 
+return 0; 
+} 
