9d5d594dfa44d44d390aca19dae578de2a43332a
AMIT MAHAJAN <39582912+amitmahajan993@users.noreply.github.com>
2018-09-20 12:33:54 +0530

    Update ConstantKeyword.cpp

--- ConstantKeyword.cpp
+++ ConstantKeyword.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main() 
 { 
 	const int var = 10; 
+	//const int var = 10; allows to change value even if variable is constant
 	cout<<&var<<endl;
 	int *ptr = (int*)&var; 
 	cout<<var<<endl;
