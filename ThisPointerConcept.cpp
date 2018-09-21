6492c4117382acd39e54e9c317956475c9542a8c
AMIT MAHAJAN <39582912+amitmahajan993@users.noreply.github.com>
2018-09-21 12:19:03 +0530

    Create ThisPointerConcept.cpp
    Concept of This Pointer and relevant scenarios

---
+++ ThisPointerConcept.cpp
@@ -0,0 +1,29 @@
+#include<iostream> 
+using namespace std; 
+  
+class Test 
+{ 
+private: 
+  int x; 
+  int y; 
+public: 
+  Test(int x = 0, int y = 0) { this->x = x; this->y = y; } 
+  Test &setX(int a) { x = a; return *this; } 
+  Test &setY(int b) { y = b; return *this;} 
+  //Invalid as return type as it is expecting return type class
+  //Test &setX(int a) { x = a; return 0; } 
+  //Test &setY(int b) { y = b; return 0;} 
+  void print() { cout << "x = " << x << " y = " << y << endl; } 
+}; 
+  
+int main() 
+{ 
+  Test obj1(5, 5); 
+  
+  // Chained function calls.  All calls modify the same object 
+  // as the same object is returned by reference 
+  obj1.setX(10).setY(20); //Function Cancatenation
+  
+  obj1.print(); 
+  return 0; 
+} 
